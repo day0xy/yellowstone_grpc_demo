3480e7f882def0d2ca9f9f02b3cdc1356cbf0c49
chris <jasonfan1337@gmail.com>
2025-06-30 17:46:16 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -90,7 +90,8 @@ async fn main() -> anyhow::Result<()> {
                 if let Some(meta) = msg.transaction.clone().and_then(|t| t.meta) {
                     let logs = meta.log_messages;
                     if let Some(create_event) = CreateEvent::parse_logs::<CreateEvent>(&logs) {
-                        info!("detect creating token: {:#?}", create_event);
+                        println!("{:?}", create_event);
+                        // info!("detect creating token: {:?}", create_event);
                     };
                 }
 
